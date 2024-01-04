a2695370630c5b9b32dd2ae45e85ecbdafdc4433
lucasemanuel <gaitonlucas7@gmail.com>
2024-01-04 18:54:57 -0300

    Backend_prueba

---
+++ index.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+const server = require("./src/server");
+//importamos la funciona para llenar la base de datos 
+
+const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
+
+conn.sync({ force: true }).then(() => {
+server.listen(PORT, async() => {
+  console.log(`Server listening on port ${PORT}`);
+  await axios.get("http://localhost:3001/countriesBd")
+})
+}).catch(error => console.error(error))
---
+++ src/models/Country.js
@@ -0,0 +1,48 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    area: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    independent:{
+      type: DataTypes.BOOLEAN,
+      allowNull:true
+    }
+
+  },{
+    timestamps: false,///para que no se generen los parametros en la base de datos no necesarios
+});
+};
